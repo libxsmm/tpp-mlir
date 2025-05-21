063f79d9467ab61e3e35b351383a763d93294c5f
Adam Siemieniuk <adam.siemieniuk@intel.com>
2025-05-21 13:22:23 +0200

    Block matmul

--- lib/TPP/Transforms/X86Vectorizer.cpp
+++ lib/TPP/Transforms/X86Vectorizer.cpp
@@ -7,12 +7,17 @@
 //===----------------------------------------------------------------------===//
 
 #include "mlir/Dialect/Arith/IR/Arith.h"
-#include "mlir/Dialect/Math/IR/Math.h"
+#include "mlir/Dialect/DLTI/DLTI.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
-#include "mlir/Dialect/Linalg/Transforms/Transforms.h"
-#include "mlir/Dialect/SCF/IR/SCF.h"
-#include "mlir/Dialect/Tensor/IR/Tensor.h"
 #include "mlir/Dialect/Linalg/IR/Linalg.h"
+#include "mlir/Dialect/Linalg/Transforms/Transforms.h"
+#include "mlir/Dialect/Math/IR/Math.h"
+#include "mlir/Dialect/SCF/IR/SCF.h"
+#include "mlir/Dialect/SCF/Transforms/TileUsingInterface.h"
+#include "mlir/Dialect/SCF/Transforms/Transforms.h"
+#include "mlir/Dialect/Tensor/IR/Tensor.h"
+#include "mlir/Dialect/Utils/IndexingUtils.h"
+#include "mlir/Dialect/Utils/StaticValueUtils.h"
 #include "mlir/Dialect/Vector/Transforms/LoweringPatterns.h"
 #include "mlir/Dialect/Vector/Transforms/VectorRewritePatterns.h"
 #include "mlir/IR/BuiltinAttributes.h"
@@ -30,12 +35,64 @@ namespace tpp {
 namespace mlir {
 namespace tpp {
 
+// template <typename IntType>
+// static SmallVector<IntType> extractVector(ArrayAttr arrayAttr) {
+//   return llvm::to_vector(llvm::map_range(
+//       arrayAttr.getAsRange<IntegerAttr>(),
+//       [](IntegerAttr attr) { return static_cast<IntType>(attr.getInt()); }));
+// }
+
+static SmallVector<OpFoldResult> getRegisterBlocks(Operation *op) {
+  auto res = dlti::query(op, {"CPU", "reg_blocks"});
+  if (failed(res))
+    return {};
+
+  auto vals = llvm::dyn_cast<ArrayAttr>(*res);
+  if (!vals)
+    return {};
+
+  return getAsOpFoldResult(vals);
+}
+
 struct VectorizeMatmul : OpRewritePattern<linalg::MatmulOp> {
   using OpRewritePattern<linalg::MatmulOp>::OpRewritePattern;
 
   LogicalResult matchAndRewrite(linalg::MatmulOp matmulOp,
                                 PatternRewriter &rewriter) const override {
-    return failure();
+    if (!matmulOp.hasPureTensorSemantics())
+      return rewriter.notifyMatchFailure(matmulOp, "expects tensor semantics");
+
+    if (matmulOp.hasDynamicShape())
+      return rewriter.notifyMatchFailure(matmulOp, "expects static shape");
+
+    SmallVector<OpFoldResult> regBlocks = getRegisterBlocks(matmulOp);
+    if (regBlocks.size() != 3)
+      return rewriter.notifyMatchFailure(matmulOp, "invalid register blocking");
+
+    scf::SCFTilingOptions options;
+    options.setTileSizes(regBlocks);
+    FailureOr<scf::SCFTilingResult> tilingRes = scf::tileUsingSCF(
+        rewriter, cast<TilingInterface>(matmulOp.getOperation()), options);
+
+    if (failed(tilingRes))
+      return rewriter.notifyMatchFailure(matmulOp, "failed to block");
+
+    rewriter.replaceOp(matmulOp, tilingRes->mergeResult.replacements);
+
+    // Apply loop peeling to split tail partial iterations and allow for
+    // canonicalization to ensure all blocked ops operate on static values. The
+    // peeling is applied in reverse order from the inner-most loop to ensure
+    // that only and all tiling loops are affected.
+    //
+    // Result is ignored as peeling can fail when tiling cleanly divides
+    // a dimension which means there is no need for peeling anyway.
+    for (LoopLikeOpInterface loop : llvm::reverse(tilingRes->loops)) {
+      scf::ForOp partialIteration;
+      (void)scf::peelForLoopAndSimplifyBounds(rewriter, cast<scf::ForOp>(loop),
+                                              partialIteration);
+    }
+
+    return success();
   }
 };
 
