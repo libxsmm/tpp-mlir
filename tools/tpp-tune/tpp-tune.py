f73990d7c4861cfbe210a463f1d0d8e251ffd8a7
Rolf Morel <rolf.morel@intel.com>
2025-06-16 14:36:09 -0700

    [Tune] Introduce tpp-tune
    Walks schedule IR, collects options (name -> values mapping), calls an
    `autotune` procedure (dummy implementation just selects first value) to
    select one value for each name, walks schedule IR again to introduce
    a param constant for the each option's value and replaces the users of
    the `transform.tune.select` op with the constant op's result.
    
    Run it as `tpp-sched | tpp-tune` where `tpp-sched` produces IR
    containing `transform.tune.select` ops.

---
+++ tools/tpp-tune/tpp-tune.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+import sys
+from pathlib import Path
+from typing import Union, Sequence, Dict
+from pprint import pprint
+
+
+# Enable automagically finding TPP-MLIR's python modules (which include
+# and extend MLIR's Python bindings).
+python_packages_path = Path(__file__).parent.parent / "python_packages"
+if python_packages_path.exists():
+    sys.path = [str(python_packages_path)] + sys.path
+
+
+from mlir import ir
+from mlir.dialects import transform
+from mlir.dialects.transform import tune as transform_tune
+
+
+def walker(f):
+    def wrapper(op: Union[ir.OpView, ir.Operation]):
+        f(op)
+        for region in op.regions:
+            for block in region.blocks:
+                for child_op in block:
+                    wrapper(child_op)
+
+    return wrapper
+
+
+def autotune(choices: Dict[str, Sequence[ir.Attribute]]) -> Dict[str, ir.Attribute]:
+    # Aint tuning easy!!
+    return {key: values[0] for key, values in choices.items()}
+
+
+file = sys.stdin
+if len(sys.argv) > 1 and sys.argv[1] != "-":
+    file = open(sys.argv[1])
+
+
+with ir.Context(), ir.Location.unknown():
+    schedule = ir.Module.parse(file.read())
+
+    choices = {}
+
+    @walker
+    def choices_finder(op):
+        if isinstance(op, transform_tune.TuneSelectOp):
+            choices[op.name] = tuple(op.options)
+
+    choices_finder(schedule.operation)
+
+    pprint(choices, stream=sys.stderr)
+
+    selected = autotune(choices)
+
+    @walker
+    def selected_rewriter(op: Union[ir.OpView, ir.Operation]):
+        if isinstance(op, transform_tune.TuneSelectOp):
+            with ir.InsertionPoint(op):
+                param = transform.param_constant(
+                    transform.AnyParamType.get(), selected[op.name]
+                )
+                for use in op.result.uses:
+                    use.owner.operands[use.operand_number] = param
+
+    selected_rewriter(schedule.operation)
+
+    print(schedule)
