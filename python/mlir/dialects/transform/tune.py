ed42b75630ff86f577e7f6956a55c8b9f5377b4a
Rolf Morel <rolf.morel@intel.com>
2025-06-25 14:58:05 -0700

    Renaming of arguments

--- python/mlir/dialects/transform/tune.py
+++ python/mlir/dialects/transform/tune.py
@@ -1,31 +1,45 @@
 from ..._mlir_libs import get_dialect_registry
-from ..._mlir_libs._tppDialects.transform.tune import register_dialect_extension, register_callback_handler
+from ..._mlir_libs._tppDialects.transform.tune import (
+    register_dialect_extension,
+    register_callback_handler,
+)
 from ..._mlir_libs._tppDialects.transform import tune
 
 tune._callback = None
 from ..._mlir_libs import _tppDialects
-_tppDialects._callback = lambda : print("callbacked")
+
+_tppDialects._callback = lambda: print("callbacked")
 
 register_dialect_extension(get_dialect_registry())
 
 from ...ir import ArrayAttr, SymbolRefAttr, Attribute, Type, Operation, Value
+from ...dialects import transform
 from .._tune_transform_ops_gen import *
 
 from collections.abc import Sequence
 from typing import Union
 
 
-def callback(results: Type, name: Union[str, Attribute], *targets: Sequence[Union[Operation, Value, Attribute]], loc=None, ip=None):
+def callback(
+    results: Type,
+    name: Union[str, Attribute],
+    *payloads: Union[
+        transform.AnyOpType, transform.AnyParamType, transform.AnyValueType
+    ],
+    loc=None,
+    ip=None
+):
     if isinstance(name, str):
         name = SymbolRefAttr.get([name])
 
     return TuneCallbackOp(
         results_=results,
         name=name,
-        targets=targets,
+        payloads=payloads,
         loc=loc,
         ip=ip,
     )
+
 
 def select(
     selected: Type,  # transform.any_param or transform.param<...>
